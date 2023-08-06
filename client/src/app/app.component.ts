b8e41cd53a7da71d4f03b7e09152d1ed97442ee1
LewisCardy <91138837+LewisCardy@users.noreply.github.com>
2023-08-06 21:37:52 +0100

    File upload button visable but not yet functional

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+  fileName = '';
+  files = [];
+  constructor(private http: HttpClient) {}
+
+    onFileSelected(event) {
+      for (let i = 0; i < this.files.length; i++) {
+        const mainDiv = document.getElementById("main-content");
+        const file:File = this.files[i];
+        const div = document.createElement('div');
+        div.textContent = file.name;
+        mainDiv.appendChild(div);
+      }
+
+        const file:File = event.target.files[0];
+
+        const formData = new FormData();
+
+        formData.append("thumbnail", file);
+
+        const upload$ = this.http.post("/api/thumbnail-upload", formData);
+
+        upload$.subscribe();
+    }
+  
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSlideToggleModule } from '@angular/material/slide-toggle'; 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
