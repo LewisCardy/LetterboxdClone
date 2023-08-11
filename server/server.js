fe58aadf60e03fe48646e00a69cf4321428a53ab
LewisCardy <91138837+LewisCardy@users.noreply.github.com>
2023-08-11 17:04:11 +0100

    Changed to multer for file upload.

--- server/server.js
+++ server/server.js
@@ -1,20 +1,31 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
+
+const PORT = process.env.PORT || 8080;
+const DATABASE = process.env.DATABASE;
   
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
+
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 
-//middleware
-const multipart = require('connect-multiparty');
-const multipartMiddleware = multipart({
-    uploadDir: './userFiles'
+const multer  = require('multer')
+
+
+var storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './userFiles');
+    },
+    filename: function (req, file, callback) {
+        callback(null, file.originalname);
+    }
 });
 
-
+const upload = multer({storage: storage}).single('file');
 
 // handling CORS
 app.use((req, res, next) => {
@@ -33,20 +44,23 @@ app.get('/api/thumbnail-upload', (req, res) => {
     res.send('Hello World')
 });
 
-app.post('/api/thumbnail-upload', multipartMiddleware,(req, res) => {
-    console.log(req.body)
-    res.json({
-        'message': 'File uploaded'
-    });
-    console.log(req.files.file.path);
+app.post('/api/thumbnail-upload', async (req, res) => {
+    upload(req, res, function (err) {
+        if (err) {
+            console.log(err)
+        } else {
+            var FileName = req.file.filename;
+            res.status(200).send('File Uploaded');
+        }
+    })
 });
 
 const start = async() => {
     try{
-        await mongoose.connect('mongodb+srv://lewiscardy2:KQi8aYrdGnkpQZIa@letterboxdstats.wob7t1b.mongodb.net/?retryWrites=true&w=majority');
+        await mongoose.connect(DATABASE);
 
-        app.listen(8080, () => {
-            console.log('Server listening on port 8080');
+        app.listen(PORT, () => {
+            console.log('Server listening on port ' + PORT);
         });
     } catch(e){
         console.log('Could not connect to database: Message', e.message)
